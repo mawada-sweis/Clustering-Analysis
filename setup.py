e8b1107681a3a341e0567817256323faecdee6a7
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2023-02-10 20:11:07 +0200

    Skeleton folders

---
+++ setup.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from setuptools import find_namespace_packages, setup
+
+# Load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+analysis_packes = ["jupyterlab==3.4.6"]
+docs_packages = ["mkdocs==1.3.0", "mkdocstrings==0.18.1"]
+# Define our package
+setup(
+    name="Clustering",
+    version=1.0,
+    description="Provide best product for each segmentation.",
+    python_requires=">=3.7",
+    packages=find_namespace_packages(),
+    install_requires=[required_packages],
+    extras_require={
+        "dev": analysis_packes,
+        "docs": docs_packages,
+    },
+)
