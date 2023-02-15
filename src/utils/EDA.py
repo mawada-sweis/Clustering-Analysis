7eabbbce22d1db96085ba4292452318b85ea0d0d
Zubaida <sdrzubaida@gmail.com>
2023-02-15 23:06:06 +0200

    Base functions for plotting

--- src/utils/EDA.py
+++ src/utils/EDA.py
@@ -1,12 +1,13 @@
 # Libraries 
 import pandas as pd
+import numpy as np 
 
 # Plotting
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plt_missing_data(data, str):
+def plt_missing_data(data, title):
     '''
     Plots a bar chart showing the percentage of missing values for each column.
     
@@ -28,8 +29,95 @@ def plt_missing_data(data, str):
 
     # Plot the percentage of missing values for each column
     plt.bar(missing_columns.index, missing_columns.values)
-    plt.title('Missing Values ' + str + ' Cleaning')
+    plt.title(title)
     plt.xlabel('Column Name')
     plt.ylabel('Percentage of Missing Values')
     plt.xticks(rotation=90)
+    plt.show()
+
+
+def create_histogram(data, bin=30, orientation='vertical', color='green', xlabel='', ylabel='', title=''):
+    """
+    Creates a histogram of the given data.
+
+    Args:
+        data (list or numpy array): The data to create a histogram.
+        bin (int): The num of bins
+        orientation (str, optional): The orientation of the histogram, either 'vertical' (default) or 'horizontal'.
+        color (str, optional): The color of the histogram bars.
+        xlabel (str, optional): The label for the x-axis.
+        ylabel (str, optional): The label for the y-axis.
+        title (str, optional): The title of the histogram.
+
+    Returns:
+        None
+    """
+    plt.hist(data, bins=bin, orientation=orientation, color=color)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+
+
+def create_boxplot(data, labels, color='green', xlabel='', ylabel='', title=''):
+    """
+    Creates a box plot of the given data.
+
+    Args:
+        data (list or numpy array): The data to create a box plot of.
+        labels (list): The labels for the x-axis.
+        color (str, optional): The color of the boxes and whiskers.
+        xlabel (str, optional): The label for the x-axis.
+        ylabel (str, optional): The label for the y-axis.
+        title (str, optional): The title of the box plot.
+
+    Returns:
+        None
+    """
+    plt.boxplot(data, labels=labels, patch_artist=True, boxprops=dict(facecolor=color))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+
+
+def create_scatterplot(x, y, xlabel='', ylabel='', title=''):
+    """
+    Creates a scatter plot of the given x and y data.
+
+    Args:
+        x (list or numpy array): The x-axis data.
+        y (list or numpy array): The y-axis data.
+        xlabel (str, optional): The label for the x-axis.
+        ylabel (str, optional): The label for the y-axis.
+        title (str, optional): The title of the scatter plot.
+
+    Returns:
+        None
+    """
+    plt.scatter(x, y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+
+
+def create_correlation_heatmap(df, title=''):
+    """
+    Creates a correlation heatmap of the given DataFrame.
+
+    Args:
+        df (pandas DataFrame): The DataFrame to create the correlation heatmap for.
+        title (str, optional): The title of the heatmap.
+
+    Returns:
+        None
+    """
+    corr = df.corr()
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+    cmap = sns.diverging_palette(230, 20, as_cmap=True)
+    sns.heatmap(corr, mask=mask, cmap=cmap, square=True, annot=True, fmt='.2f',
+                cbar_kws={"shrink": .8}, linewidths=.5, center=0, vmax=1, vmin=-1,
+                annot_kws={"fontsize":8})
+    plt.title(title)
     plt.show()
