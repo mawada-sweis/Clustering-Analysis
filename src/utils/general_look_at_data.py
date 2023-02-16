6d9cf1d4ffcb8293c9188b513e0e385541cd628a
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2023-02-16 20:42:40 +0200

    Refactoring Code

--- src/utils/general_look_at_data.py
+++ src/utils/general_look_at_data.py
@@ -1,29 +1,44 @@
 def get_feature_types_stats(df):
-    features = df.dtypes.rename('feature_type').reset_index().rename(columns={'index':'feature'})
-    return features 
+    """
+    Computes the data types of features in a DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing the following columns:
+        - feature: the name of the feature
+        - feature_type: the data type of the feature
+    """
+    # Compute the data types of features in the DataFrame
+    return df.dtypes.rename('feature_type').reset_index().rename(columns={'index': 'feature'})
 
 
-def get_feature_missing(df): 
-    df_rows = df.shape[0]
+def get_feature_missing(df):
+    """
+    Computes missing value statistics for each feature in a DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing the missing data statistics columns:
+    """
+    # Compute missing value counts and percentages for each feature
     missing_stats = df.isna().sum().rename('missing_count').reset_index()
-    dont_know = df[df == 998].notna().sum().rename('dont_know_count').reset_index()
-    no_response = df[df == -9998].notna().sum().rename('no_response_count').reset_index()
-    technological_error = df[df == 9999].notna().sum().rename('technological_error_count').reset_index()
-    prefer_not_to_answer = df[df == 999].notna().sum().rename('prefer_not_to_answer_count').reset_index()
-    not_required = df[df == 995].notna().sum().rename('not_required_count').reset_index()
-    
-    missing_stats['missing_percentage'] = missing_stats['missing_count']/df_rows
-    missing_stats['dont_know_percentage'] = dont_know['dont_know_count']/df_rows
-    missing_stats['no_response_percentage'] = no_response['no_response_count']/df_rows
-    missing_stats['technological_error_percentage'] = technological_error['technological_error_count']/df_rows 
-    missing_stats['not_required_percentage'] = not_required['not_required_count']/df_rows
-    missing_stats['prefer_not_to_answer_percentage'] = prefer_not_to_answer['prefer_not_to_answer_count'] /df_rows
-    
+    missing_stats['missing_percentage'] = missing_stats['missing_count'] / len(df)
+
+    # Compute counts and percentages for each value category
+    counts = {}
+    for val, label in [(998, 'dont_know'), (-9998, 'no_response'), (9999, 'technological_error'), (999, 'prefer_not_to_answer'), (995, 'not_required')]:
+        counts[label] = df[df == val].notna().sum().rename(f'{label}_count').reset_index()
+        missing_stats[f'{label}_percentage'] = counts[label][f'{label}_count'] / len(df)
+
+    # Add feature type information
     feature_types_df = get_feature_types_stats(df)
-    
-    missing_stats = missing_stats.rename(columns={'index':'feature'}).merge(feature_types_df,
-                         left_on='feature',
-                         right_on='feature',
-                         how='left')
-    missing_stats.sort_values('missing_percentage', ascending=False)
-    return missing_stats 
+    missing_stats = missing_stats.rename(columns={'index': 'feature'}).merge(feature_types_df,
+                                                                             on='feature',
+                                                                             how='left')
+
+    # Sort the results by descending order of missing_percentage
+    return missing_stats.sort_values('missing_percentage', ascending=False)
