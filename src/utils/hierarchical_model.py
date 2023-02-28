78aaa3c44a86d5bcebbe958621ba869438720060
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2023-03-01 00:36:47 +0200

    hierarchy Clustering after and before reduction

--- src/utils/hierarchical_model.py
+++ src/utils/hierarchical_model.py
@@ -2,11 +2,30 @@ from numpy import ndarray
 import pandas as pd
 from scipy.cluster.hierarchy import dendrogram, linkage
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from scipy.cluster.hierarchy import fcluster
+
+# Define a list of colors to choose from
+colors_code = ['#609EA2', '#AA5656', '#F2921D', "#4E6C50"]
+
+
+def get_linkage(data: pd.DataFrame, method_type='ward', distance_metric='euclidean', optimal_ordering=False) -> ndarray:
+    """Calculates hierarchical clustering linkage matrix for a given dataset.
+
+    Args:
+        data (pandas.DataFrame): The dataset to cluster.
+        method_type (str, optional): The clustering algorithm to use. Default is 'ward'.
+        distance_metric (str, optional): The distance metric to use. Default is 'euclidean'.
+        optimal_ordering (bool, optional): Whether to compute the optimal leaf ordering for the dendrogram. Default is False.
+
+    Returns:
+        linkage_matrix (numpy.ndarray): The hierarchical clustering linkage matrix.
+    """
+    return linkage(data, method=method_type, metric=distance_metric, optimal_ordering=optimal_ordering)
 
 
 def plot_dendrogram_all_methods(data: pd.DataFrame, method_list=None, distance_metric='euclidean', optimal_ordering=False) -> None:
-    """
-    Calculates the linkage matrix and draws the dendrogram plot for each given linkage method.
+    """Calculates the linkage matrix and display the dendrogram plot for each given linkage method.
 
     Args:
         data (pd.DataFrame): The dataset to draw the dendrogram plot of.
@@ -17,47 +36,36 @@ def plot_dendrogram_all_methods(data: pd.DataFrame, method_list=None, distance_m
 
     # Set default linkage methods if not provided
     if method_list is None:
-        method_list = [
-            'single',
-            'complete',
-            'average',
-            'weighted',
-            'centroid',
-            'median',
-            'ward',
-        ]
+        method_list = ['single', 'complete', 'average', 'weighted', 'centroid', 'median', 'ward']
 
     # Create a figure to plot the dendrograms
     fig, axs = plt.subplots(len(method_list), 1, figsize=(10, 30), sharex=True, gridspec_kw={'hspace': 0.5})
 
     # Loop over the methods and plot the corresponding dendrogram
     for index, method in enumerate(method_list):
-        linkage_matrix = linkage(data, method=method, metric=distance_metric, optimal_ordering=optimal_ordering)
+        # Calculate the linkage matrix
+        linkage_matrix = get_linkage(data, method, distance_metric, optimal_ordering)
+        
+        # Plot the dendrogram
         dendrogram(linkage_matrix, ax=axs[index])
+        
+        # Set the plot title and axis labels
         axs[index].set_title(f'Dendrogram ({method} linkage)')
         axs[index].set_xlabel('Sample Index')
         axs[index].set_ylabel('Distance')
-
+    
+    # Show the plot and return the dendrogram object
     plt.show()
 
 
-def display_dendrogram(data: pd.DataFrame, method_type='ward', distance_metric='euclidean', optimal_ordering=False) -> ndarray:
-    """
-    Calculates the linkage matrix and draws the dendrogram plot.
+def display_dendrogram(linkage_matrix: pd.DataFrame, method_type='ward') -> None:
+    """Display the dendrogram plot given a linkage matrix and a linkage method. 
+    The dendrogram plot shows the hierarchical clustering of the dataset.
 
     Args:
-        data (pd.DataFrame): The dataset to draw the dendrogram plot of.
-        method_list (str): The linkage method to use. Defaults to 'ward'.
-        distance_metric (str): The distance metric to use when calculating the linkage matrix. Defaults to 'euclidean'.
-        optimal_ordering (bool): Whether to use optimal leaf ordering when drawing the dendrogram plot. Defaults to False.
-
-    Returns:
-        scipy.cluster.hierarchy.dendrogram: The dendrogram object.
+        linkage_matrix (pd.DataFrame): The linkage matrix of the dataset.
+        method_type (str): The linkage method to use. Defaults to 'ward'.
     """
-
-    # Calculate the linkage matrix
-    linkage_matrix = linkage(data, method=method_type, metric=distance_metric, optimal_ordering=optimal_ordering)
-
     # Plot the dendrogram
     dendrogram(linkage_matrix)
 
@@ -66,22 +74,150 @@ def display_dendrogram(data: pd.DataFrame, method_type='ward', distance_metric='
     plt.xlabel('Sample Index')
     plt.ylabel('Distance')
 
-    # Show the plot and return the dendrogram object
+    # Show the plot
     plt.show()
-    return linkage_matrix
 
 
 def plot_thresholded_dendrogram(linkage_matrix: ndarray, distance_threshold: int) -> None:
-    """
-    Plots the dendrogram with a horizontal line at the given threshold.
+    """Plots the dendrogram with a horizontal line at the given threshold.
 
     Args:
-        linkage_matrix (numpy.ndarray): The linkage matrix.
+        linkage_matrix (numpy.ndarray): The linkage matrix of the dataset.
         threshold (float): The threshold distance to plot the horizontal line at.
     """
-
+    # Plot the dendrogram
     plt.figure(figsize=(10, 7))
-    plt.title("Dendrogram with Threshold")
     dendrogram(linkage_matrix)
+    
+    # Set the plot title and axis labels
+    plt.title("Dendrogram with Threshold")
+    
+    # Draw the horizontal line
     plt.axhline(y=distance_threshold, color='black', linestyle='--')
+    
+    # Show the plot
+    plt.show()
+
+
+def hierarchical_clustering(linkage_matrix: ndarray, k: int, criterion='maxclust') -> ndarray:
+    """To cluster one-hot encoded data using fcluster from hierarchical clustering algorithms.
+
+    Args:
+        linkage_matrix (ndarray): The linkage matrix of the dataset.
+        k (int): number of clusters.
+        criterion (str, optional): The criterion to use in forming flat clusters. Defaults to 'maxclust'.
+
+    Returns:
+        ndarray: An array of length n. T[i] is the flat cluster number to which original observation i belongs.
+    """
+    return fcluster(linkage_matrix, k, criterion)
+
+
+def _map_color_cluster(df: pd.DataFrame) -> pd.Series(str):
+    """Assigns a color to each cluster in the given DataFrame based on their label.
+    Args:
+        df (pd.DataFrame): The DataFrame containing the cluster labels.
+
+    Returns:
+        pd.Series(str): A list of colors, where the i-th element corresponds to the color of the i-th row in the DataFrame.
+    """
+    # Get the unique cluster labels from the DataFrame
+    cluster_labels = df['cluster'].unique()
+
+    # Assign a color to each cluster label
+    cluster_colors = {label: colors_code[i] for i, label in enumerate(cluster_labels)}
+
+    # Create a new column in the DataFrame to store the cluster colors
+    return df['cluster'].apply(lambda x: cluster_colors[x])
+
+
+def get_transform_PCA(data: pd.DataFrame, dimensions: int) -> ndarray:
+    """Performs principal component analysis (PCA) on a given pandas DataFrame data
+        and returns the transformed data.
+
+    Args:
+        data (pd.DataFrame): The input data to be transformed using PCA.
+        dimensions (int): The number of dimensions to keep after the transformation.
+    Returns:
+        ndarray: A numpy ndarray of shape (n_samples, dimensions) representing the transformed data.
+    """
+    # Perform PCA on the data
+    pca = PCA(n_components=dimensions)
+    return pca.fit_transform(data)
+
+
+def plot_clustering_before_reduction(data: pd.DataFrame, dimensions: int) -> None:
+    """Generates a scatter plot of the clustering of data before reducing its dimensionality using PCA.
+
+    Args:
+        data (pd.DataFrame): The input dataset to be plotted.
+        dimensions (int): The number of dimensions to reduce the data to before plotting.
+    Raises:
+        ValueError: To check the requested dimensions are either 2 or 3.
+    """
+    # Check the requested dimensions
+    if dimensions not in [2, 3]:
+        raise ValueError("dimensions must be either 2 or 3")
+    
+    # Perform PCA on the data
+    transformed = get_transform_PCA(data, dimensions)
+    
+    # Assigns a color to each cluster in the given DataFrame based on their label.
+    colors = _map_color_cluster(data)
+    
+    # Set up plot
+    fig = plt.figure()
+    
+    # Plot scatter by PCA transformed data
+    if dimensions == 2:
+        ax = fig.add_subplot(111)
+        ax.scatter(transformed[:, 0], transformed[:, 1], c=colors)
+    else:
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(transformed[:, 0], transformed[:, 1], transformed[:, 2], c=colors)
+        ax.set_zlabel('PC3')
+
+    # Set axis labels and title
+    ax.set_title(f'{dimensions}-Dimension Clustering before PCA')
+    ax.set_xlabel('PC1')
+    ax.set_ylabel('PC2')
+    
+    # Show the plot
+    plt.show()
+
+
+def plot_clustering_after_reduction(data: pd.DataFrame, transformed: ndarray, dimensions: int) -> None:
+    """Generates a scatter plot of the clustering of data after reducing using the given transformed data by PCA.
+
+    Args:
+        data (pd.DataFrame): The input dataset to be plotted.
+        transformed (ndarray): The transformed data using PCA. 
+        dimensions (int): The number of dimensions to reduce the data to after plotting.
+    Raises:
+        ValueError: To check the requested dimensions are either 2 or 3.
+    """
+    # Check the requested dimensions
+    if dimensions not in [2, 3]:
+        raise ValueError("dimensions must be either 2 or 3")
+    
+    # Assigns a color to each cluster in the given DataFrame based on their label.
+    colors = _map_color_cluster(data)
+    
+    # Set up plot
+    fig = plt.figure()
+    if dimensions == 2:
+        ax = fig.add_subplot(111)
+        ax.scatter(transformed[:, 0], transformed[:, 1], c=colors)
+    
+    else:
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(transformed[:, 0], transformed[:, 1], transformed[:, 2], c=colors)
+        ax.set_zlabel('PC3')
+
+    # Set axis labels and title
+    ax.set_title(f'{dimensions}-Dimension Clustering after PCA')
+    ax.set_xlabel('PC1')
+    ax.set_ylabel('PC2')
+    
+    # Show the plot
     plt.show()
